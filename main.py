ed81015104b3310f2b94503337d342d4c12c84f1
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-10 13:26:00 +0000

    added solutions

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #Write your code below this row 👇
 
+even = 0
+for number in range(1, 103, 2):
+  # print(number-1)
+  even += number-1
+print(even)
+
+even2 = 0
+for number in range(2, 101, 2):
+  even2 += number
+print(even2)
+
+even3 = 0
+for number in range(1, 101):
+  if number % 2 == 0:
+    even3 += number
+print(even3)
